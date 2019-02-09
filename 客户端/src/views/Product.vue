83c3d1f6a1a9305ac97d3acdf286296dfdc5d0a7
QFL <16602117410@163.com>
2019-02-09 12:24:07 +0800

    客户端

---
+++ 客户端/src/views/Product.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="app-product">
+      <Header></Header>
+      <router-view></router-view>
+      <Footer></Footer>
+    </div>
+</template>
+<script>
+import Header from '../components/Header.vue';
+import Footer from '../components/Footer.vue';
+  export default {
+    components:{
+      Header,Footer
+    }
+  }
+</script>
+
